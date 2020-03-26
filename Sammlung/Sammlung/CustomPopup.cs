2318d432c6a2fa4c3466cb8d05be0a1864ef8748
Hung <hungdangquoc95@googlemail.com>
2020-03-26 10:45:05 +0100

    Projekt
    gesamte Sammlung Solution

---
+++ Sammlung/Sammlung/CustomPopup.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Sammlung
+{
+    public class CustomPopup : UserControl
+    {
+        private StackPanel area;
+        public Action Checked;
+        public Dictionary<string, bool> states;
+        public Dictionary<string, Border> checkboxes;
+
+        public CustomPopup()
+        {
+            area = new StackPanel();
+            states = new Dictionary<string, bool>();
+            checkboxes = new Dictionary<string, Border>();
+            ScrollViewer scrollViewer = new ScrollViewer();
+            scrollViewer.VerticalScrollBarVisibility = ScrollBarVisibility.Hidden;
+            scrollViewer.Content = area;
+            this.AddChild(scrollViewer);
+
+            AddSearchElement();
+            AddElement("Alle", "#FF1D1D1D");
+        }
+
+        private void AddSearchElement()
+        {
+            Border border = new Border();
+            TextBox TextBox = new TextBox();
+            TextBox.TextChanged += new TextChangedEventHandler(((object sender, TextChangedEventArgs e) => {
+                foreach (string state in states.Keys.ToArray())
+                {
+                    if (!state.ToLower().Contains(TextBox.Text.ToLower()))
+                    {
+                        checkboxes[state].Visibility = Visibility.Hidden;
+                        checkboxes[state].Height = 0;
+                    }
+                    else
+                    {
+                        checkboxes[state].Visibility = Visibility.Visible;
+                        checkboxes[state].Height = Double.NaN;
+                    }
+                }
+            }));
+            border.Child = TextBox;
+            area.Children.Add(border);
+        }
+
+        public void AddElement(string name)
+        {
+            AddElement(name, "#FF0E0E0E");
+        }
+
+        public void AddElement(string name, string color)
+        {
+            Border border = new Border();
+            CheckBox checkBox = new CheckBox();
+            TextBlock label = new TextBlock();
+            label.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString(color));
+            label.Text = name;
+            checkBox.Content = label;
+            checkBox.IsChecked = true;
+            checkBox.Checked += new RoutedEventHandler((object sender, RoutedEventArgs e) => ChangeState(name));
+            checkBox.Unchecked += new RoutedEventHandler((object sender, RoutedEventArgs e) => ChangeState(name));
+            border.Child = checkBox;
+            states.Add(name, true);
+            area.Children.Add(border);
+            checkboxes.Add(name, border);
+        }
+
+        private void ChangeState(string name)
+        {
+            if (name == "Alle")
+            {
+                int StatesChecked = 0;
+                int StatesUnChecked = 0;
+                foreach (bool check in states.Values.ToArray())
+                {
+                    if (check)
+                    {
+                        StatesChecked++;
+                    }
+                    else
+                    {
+                        StatesUnChecked++;
+                    }
+                }
+                foreach (Border b in checkboxes.Values.ToArray())
+                {
+                    ((CheckBox)b.Child).IsChecked = StatesChecked < StatesUnChecked;
+                }
+                Checked();
+            }
+            else
+            {
+                states[name] = !states[name];
+                Checked();
+            }
+        }
+
+        public Dictionary<string, bool> GetStates()
+        {
+            return states;
+        }
+    }
+}
---
+++ Sammlung/Sammlung/MainWindow.xaml.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sammlung
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //FILTER
+        CustomPopup boardFilter;
+        CustomPopup commandFilter;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            boardFilter = new CustomPopup();
+            FilterBoard.Child = boardFilter;
+
+            commandFilter = new CustomPopup();
+            FilterCommand.Child = commandFilter;
+
+            for (int i = 0; i < 25; i++)
+            {
+                boardFilter.AddElement(i.ToString(), "#FFFFFFFF");
+            } 
+            
+            for (int i = 25; i > 0; i--)
+            {
+                commandFilter.AddElement(i.ToString(), "#FFFFFFFF");
+            }
+        }
+
+
+        private void hashtable()
+        {
+            System.Collections.Hashtable h = new System.Collections.Hashtable();
+
+            h.Add("a", "Banane");
+            h.Add("b", "Erdbeere");
+            h.Add("c", "Orange");
+        }
+
+        int i = 0;
+        private void RepeatButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.Primitives.RepeatButton button = sender as System.Windows.Controls.Primitives.RepeatButton;
+
+            button.Content = i++;
+        }
+
+        private void treeviewer()
+        {
+            foreach (string str in Environment.GetLogicalDrives())
+            {
+                TreeViewItem item = new TreeViewItem();
+                item.Header = str;
+                item.Tag = str;
+                item.Items.Add(null);
+                treeView.Items.Add(item);
+            }
+        }
+
+        private void treeViewItem_Expanded(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = e.OriginalSource as TreeViewItem;
+
+            if (item != null)
+            {
+                if (item.Items.Count == 1 && item.Items[0] == null)
+                {
+                    item.Items.Clear();
+                    try
+                    {
+                        foreach (string str in Directory.GetDirectories(item.Tag.ToString()))
+                        {
+                            TreeViewItem subitem = new TreeViewItem();
+                            subitem.Header = str.Substring(str.LastIndexOf('\\') + 1);
+                            subitem.Tag = str;
+                            subitem.Items.Add(null);
+                            item.Items.Add(subitem);
+                        }
+                    }
+                    catch (UnauthorizedAccessException ex) { }
+                    catch (InvalidOperationException ex) { }
+                }
+            }
+        }
+
+        private void InitDatagrid()
+        {
+            List<GridviewPerson> gvp = new List<GridviewPerson>();
+            gvp.Add(new GridviewPerson() { FirstName = "Hung", LastName = "DangQuoc", Birthday = DateTime.Now});
+            gvp.Add(new GridviewPerson() { FirstName = "Lea", LastName = "Michel-Döbler", Birthday = DateTime.Now});
+            gvp.Add(new GridviewPerson() { FirstName = "Nam", LastName = "DangQuoc", Birthday = DateTime.Now});
+            gvp.Add(new GridviewPerson() { FirstName = "Kevin", LastName = "Koch", Birthday = DateTime.Now});
+
+            foreach (var item in gvp)
+            {
+                dataGrid.Items.Add(item);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            treeviewer();
+            InitDatagrid();
+            initProgbar();
+            createImage();
+
+        }
+
+        private Image createImage()
+        {
+            string filepath = System.IO.Path.Combine(@"C:\Users\Surface\source\repos\Sammlung\Sammlung\pic\bsp.jpg");
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+            bi.UriSource = new Uri(filepath, UriKind.Absolute);
+            bi.EndInit();
+
+            Image img = new Image();
+            img.Source = bi;
+
+            return img;
+        }
+
+        private void initProgbar()
+        {
+            progbar.Value = 150;
+            progbartext.Content = "75%";
+        }
+
+        private void punkte_dataGrid_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            GridviewPerson RowData = (GridviewPerson)e.Row.Item;
+
+            switch (RowData.FirstName)
+            {
+                case "Lea": e.Row.Background = new SolidColorBrush(Colors.Green); break;
+                case "Nam": e.Row.Background = new SolidColorBrush(Colors.Blue); break;
+                case "Kevin": e.Row.Background = new SolidColorBrush(Colors.Red); break;
+                default:
+                    break;
+            }
+            
+        }
+
+        private void FilterBoardButton_Click(object sender, RoutedEventArgs e)
+        {
+            FilterBoard.IsOpen = !FilterBoard.IsOpen;
+            FilterCommand.IsOpen = false;
+        }
+
+        private void FilterCommandButton_Click(object sender, RoutedEventArgs e)
+        {
+            FilterCommand.IsOpen = !FilterCommand.IsOpen;
+            FilterBoard.IsOpen = false;
+        }
+    }
+}
